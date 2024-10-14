4648de13fe600017a00a77412b5a07ccb9b6280a
FedYou <federicochurruarin0@gmail.com>
2024-10-13 22:57:12 -0300

    perf: Now the custom console function is used in console.log by default

--- src/utils/console/index.js
+++ src/utils/console/index.js
@@ -1,7 +1,6 @@
 import "colors";
 import error from "./error.js";
-import log from "./log.js";
 import face from "./face.js";
 import _interface from "./interface.js";
 
-export default { error, log, face, interface: _interface };
+export default { error, log: console.log, face, interface: _interface };
