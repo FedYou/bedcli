9a91bb9a0d8a2f2d148fa4ffa842d68b24c0bac2
FedYou <federicochurruarin0@gmail.com>
2024-10-22 20:40:32 -0300

    refactor: :fire: Code replaced to use youfile.json5

--- src/compile/json.js
+++ src/compile/json.js
@@ -1,25 +1,6 @@
-import fs from "fs-extra";
-import Parse from "fast-json-parse";
-import console from "../utils/console/index.js";
 import youfile from "youfile";
-const regex = /("([^"\\]|\\.)*"|'([^'\\]|\\.)*'|\/\/.*|\/\*[\s\S]*?\*\/)/g;
 
 export default async (entry, output) => {
-  const fileContent = fs.readFileSync(entry, "utf-8");
-  let json = new Parse(fileContent);
-
-  if (json.err) {
-    const content = fileContent.replace(regex, (match) => {
-      if (match.startsWith('"') || match.startsWith("'")) {
-        return match;
-      }
-      return "";
-    });
-
-    json = new Parse(content);
-    if (json.err) {
-      console.error("unable to parse json", json.err.message);
-    }
-  }
-  youfile.write.json(output, json.value);
+  const json = youfile.read.json5(entry);
+  youfile.write.json(output, json);
 };
